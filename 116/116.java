9a6b9cea894fec4bd6f74f08f54c8a80fe61c4e3
Arini11 <elarnaumc@gmail.com>
2020-12-13 13:04:55 +0100

    116

---
+++ 116/116.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ac{
+	public static void main(String[] args){
+		Scanner lector=new Scanner(System.in);
+		int n = lector.nextInt();
+		while(n>0){
+			System.out.println("Hola mundo.");
+			n--;
+		}
+	}
+}
